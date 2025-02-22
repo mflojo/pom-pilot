2d56576b2540b687a53f122a28dfcfb39ad01664
mflojo <m.eric.flojo@gmail.com>
2025-02-21 21:27:11 -0800

    Add event listeners for both card buttons

--- src/index.js
+++ src/index.js
@@ -5,13 +5,26 @@ import { input } from "./input.js";
 console.log("Testing js, this is the DOM loaded");
 
 const button = document.querySelector("#add-task");
-const dialog = document.querySelector("#user-input");
 
 button.addEventListener("click", () => {
     console.log("you clicked on the Add New Task button!");
     input();
-    
+});
 
+const screen = document.querySelector("#screen");
 
-    
+screen.addEventListener("click", function(event) {
+    if (event.target.classList.contains("card-delete")) {
+        const card = event.target.closest(".card");
+        if (card) {
+            card.remove();  // Remove the card
+        }
+    }
+});
+
+screen.addEventListener("click", function(event) {
+    if (event.target.classList.contains("card-complete")) {
+        const card = event.target.closest(".card");
+        card.classList.toggle("completed-task");
+    }
 });
