013893a795a821fe0f1798eb0e6c7e98e6b7bd66
mflojo <m.eric.flojo@gmail.com>
2025-02-21 19:47:04 -0800

    Add event listener to button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./styles.css";
+import { input } from "./input.js";
+
+
+console.log("Testing js, this is the DOM loaded");
+
+const button = document.querySelector("#add-task");
+const dialog = document.querySelector("#user-input");
+
+button.addEventListener("click", () => {
+    console.log("you clicked on the Add New Task button!");
+    input();
+    
+
+
+    
+});
